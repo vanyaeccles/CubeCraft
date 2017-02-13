3015477e381e806fb80b53ece805da26aca33e49
Daniel Cullen <cullend6@tcd.ie>
2017-02-13 17:30:26 +0000

    first lesson from mr. Dad

---
+++ cubecraft/Assets/Grid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    private Vector2[,] grid;
+    public int size = 4;
+    public float offset = 1.0f;
+
+    void Awake()
+    {
+        grid = new Vector2 [size,size];
+    }
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < size; j++)
+            {
+                grid[i,j] = new Vector2(i*offset,j*offset);
+            }
+        }
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
