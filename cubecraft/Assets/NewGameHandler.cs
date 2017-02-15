d99ac1407459517d9ba0985cb97dbee663bbf7c7
ITSERV\dillonse <dillonse@tcd.ie>
2017-02-15 16:49:45 +0000

    implemented loading from the new game button

--- cubecraft/Assets/NewGameHandler.cs
+++ cubecraft/Assets/NewGameHandler.cs
@@ -2,23 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class NewGameHandler : MonoBehaviour {
+public class NewGameHandler: MonoBehaviour {
     public Button StartNewGameButton;
 	// Use this for initialization
 	void Start () {
         Button btn = StartNewGameButton.GetComponent<Button>();
         btn.onClick.AddListener(StartNewGame);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     void StartNewGame()
     {
         Debug.Log("start new game!");
-
+        SceneManager.LoadScene("GameplayScene",LoadSceneMode.Single); //Single vs Additive, single will close other scenes, additive will not
     }
 }
