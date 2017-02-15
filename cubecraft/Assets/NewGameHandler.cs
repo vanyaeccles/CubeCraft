7d5a4c9924378d87361283f28be845095877632f
ITSERV\dillonse <dillonse@tcd.ie>
2017-02-15 16:05:11 +0000

    started the UI handling functionalities

---
+++ cubecraft/Assets/NewGameHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewGameHandler : MonoBehaviour {
+    public Button StartNewGameButton;
+	// Use this for initialization
+	void Start () {
+        Button btn = StartNewGameButton.GetComponent<Button>();
+        btn.onClick.AddListener(StartNewGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartNewGame()
+    {
+        Debug.Log("start new game!");
+
+    }
+}
