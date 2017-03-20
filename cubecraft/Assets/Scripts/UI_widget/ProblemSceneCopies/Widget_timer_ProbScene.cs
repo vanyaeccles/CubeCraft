a4714715e366ec5f764c0b38382503de45ca3df9
ITSERV\ecclesva <ecclesva@tcd.ie>
2017-03-20 20:32:45 +0000

    Added camera rotation and timer to problem scene, though these could be positioned on the screen somewhere better maybe

---
+++ cubecraft/Assets/Scripts/UI_widget/ProblemSceneCopies/Widget_timer_ProbScene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Widget_timer_ProbScene : MonoBehaviour {
+
+    public UIHandler_ProbScene uiHandler;
+    public GameObject panel;
+    public Text timer_text;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void setTimerText(string text)
+    {
+        timer_text.text = text;
+    }
+
+    public void SetTime(string minutes, string seconds)
+    {
+        timer_text.text = minutes + ":" + seconds;
+    }
+
+
+    public void SetTimerTextRed()
+    {
+        //@TODO this changes the colour of all 'Sprites-Default', dont do it
+        //timer_text.material.color = Color.white;
+    }
+}
