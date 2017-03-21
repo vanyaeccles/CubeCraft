2f7a9e699fd54acca5ec107809f075fcd45e9287
sakumar <sakumar@tcd.ie>
2017-03-21 12:29:57 +0000

    Start Menu > Settings > Volume Controls (Menu Manager - Script - Menu.cs)

---
+++ cubecraft/Assets/Scripts/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+    public GameObject menuholder;
+    public GameObject settingmenuholder;
+    public Slider[] VolumeSliders;
+    public Toggle[] ScreenResolution;
+  
+
+    public void settingsmenu()
+    {
+        menuholder.SetActive(false);
+        settingmenuholder.SetActive(true);
+    }
+
+    public void mainmenu()
+    {
+        menuholder.SetActive(true);
+        settingmenuholder.SetActive(false);
+    }
+
+    public void setFullscreen(bool isFullscreen)
+    {
+
+    }
+    public void setMasterVolume(float value)
+    {
+       // AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
+    }
+    public void setMusicVolume(float value)
+    {
+      //  AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
+    }
+    public void setSoundFXVolume(float value)
+    {
+      //  AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.SoundFx);
+    }
+
+
+   
+
+
+}
