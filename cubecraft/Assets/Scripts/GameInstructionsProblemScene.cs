bac4e7ece883b1fa573abfb21fe89afe1935db6d
sakumar <sakumar@tcd.ie>
2017-03-21 16:01:27 +0000

    Simple Way of Instructions - As a back up

---
+++ cubecraft/Assets/Scripts/GameInstructionsProblemScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstructionsProblemScene : MonoBehaviour {
+    private string camInst = "";
+
+    private bool show = false;
+
+    // Use this for display your text on monitor
+    void Start()
+    {
+        camInst = "";
+        camInst += "- Click The Camera Arrows to Rotate the Puzzle ";
+        camInst += "- and remeber clock is ticking\n";
+        camInst += "\n iff you read this text you rule dude \n";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+
+        if (GUI.Button(new Rect(Screen.width - 720, 40, 35, 35), "!"))
+        {
+            show = !show;
+        }
+
+        if (show) GUI.Label(new Rect(Screen.width - 680, 50, 300, 500), camInst);
+    }
+}
