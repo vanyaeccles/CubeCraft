a825b3c31a610c07ec5eeaae5aca1d5a78e5f894
VLERY-PC\vlery <liu>
2017-03-24 13:01:11 +0000

    add tutorial panel

--- cubecraft/Assets/Scripts/MenuUIHandler.cs
+++ cubecraft/Assets/Scripts/MenuUIHandler.cs
@@ -9,14 +9,25 @@ public class MenuUIHandler: MonoBehaviour {
 
 
     public Button StartNewGameButton;
+
+    public Button HelpButton;
+    public GameObject tutorialPanel;
+
 	// Use this for initialization
 	void Start () {
         Button btn = StartNewGameButton.GetComponent<Button>();
         btn.onClick.AddListener(StartNewGame);
+        HelpButton.onClick.AddListener(processHelpEvent);
+        tutorialPanel.SetActive(false);
 	}
 
     void StartNewGame()
     {
         GameObject.Find("SceneManager").GetComponent<SceneLoader>().LoadLevelsScene();
+    }
+
+    void processHelpEvent()
+    {
+        tutorialPanel.GetComponent<Widget_tutorial_panel>().show();
     }
 }
