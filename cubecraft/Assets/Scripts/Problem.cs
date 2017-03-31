8f14d81e821f38acaee54a50499ccf53673efd74
ITSERV\dillonse <dillonse@tcd.ie>
2017-03-31 15:32:31 +0100

    Hardcoded JSONs for Device deployment

--- cubecraft/Assets/Scripts/Problem.cs
+++ cubecraft/Assets/Scripts/Problem.cs
@@ -18,13 +18,31 @@ public class Problem{
 
     public static Problem Load(string filename)
     {
+       
         //Debug.Log("SA is " + Application.streamingAssetsPath);
         string path = Application.streamingAssetsPath + "/Levels/" + filename;
         //Debug.Log("path iz " + path);
         Problem problem;
         try
         {
-            string json_str = System.IO.File.ReadAllText(path);
+            //**HACK FOR DEVICES**//
+            string json_str;
+            if (filename == "Level2x2A.JSON")
+                json_str = HardcodedLevels.level1;
+            else if (filename == "Level2x2B.JSON")
+                json_str = HardcodedLevels.level2;
+            else if (filename == "Level2x2C.JSON")
+                json_str = HardcodedLevels.level3;
+            else if (filename == "Level3x3A.JSON")
+                json_str = HardcodedLevels.level4;
+            else if (filename == "Level3x3B.JSON")
+                json_str = HardcodedLevels.level5;
+            else if (filename == "Level3x3C.JSON")
+                json_str = HardcodedLevels.level6;
+            else
+                json_str = HardcodedLevels.level1;
+            //**END OF HACK**//
+            //string json_str = System.IO.File.ReadAllText(path);
             //Debug.Log("Loaded from " + path);
             problem = JsonUtility.FromJson<Problem>(json_str);
         }
